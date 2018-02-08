d2cbe87d58bb198ff69ade44c62525aabdc4b232
Bill Mill <bill@billmill.org>
2018-02-08 12:15:58 -0500

    add dotnet, rearrange comments, move python out of provision

--- provision.sh
+++ provision.sh
@@ -11,23 +11,34 @@ add-apt-repository ppa:gophers/archive
 add-apt-repository ppa:nginx/stable
 add-apt-repository ppa:ondrej/php
 curl -sL https://deb.nodesource.com/setup_9.x | bash -
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+
+# dotnet instructions modified from
+# https://www.microsoft.com/net/learn/get-started/linuxubuntu
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-artful-prod artful main" | tee /etc/apt/sources.list.d/dotnetdev.list
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
 
 apt-get update
 
-# Current dev environments for:
-#   * python 2.7
-#   * python 3.6
-#   * ruby 2.4
+# * Current dev environments for:
+#   * dotnet 2.1.4
 #   * golang 1.9
+#   * java 8 (openjdk)
 #   * node 1.9
 #   * php 7.2
-#   * java 8
+#   * python 2.7.14
+#   * python 3.6.4
+#   * ruby 2.5
 #
-# Also:
+# * Also:
 #   * ansible
+#   * bundler
+#   * maven
 #   * nginx
+#   * pyenv
 #   * runit
 #   * yarn
 #   * some basics for building packages
@@ -35,6 +46,7 @@ apt-get install -y git bash-completion make build-essential libssl-dev \
   zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget llvm \
   libncurses5-dev libncursesw5-dev xz-utils tk-dev unzip \
   ansible \
+  dotnet-sdk-2.1.4 \
   golang-1.9-go \
   openjdk-8-jdk maven \
   nodejs yarn \
@@ -49,10 +61,3 @@ gem install bundler
 mkdir -p ~/go
 ln -sf /usr/lib/go-1.9/bin/gofmt /usr/bin/
 ln -sf /usr/lib/go-1.9/bin/go /usr/bin/
-
-# Use pyenv to install python 3.6.4. The apt-based solutions I tried really stunk.
-curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
-echo 'export PATH="/home/vagrant/.pyenv/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
-echo 'source ~/.bashrc"' >> ~/.bash_profile
