256530fbe17d4e4f05e845c56c16a04e055eda15
Robert DiMartino <robert.dimartino@adhocteam.us>
2018-05-10 08:45:18 -0400

    move GOPATH creation to user_provision.sh

--- user_provision.sh
+++ user_provision.sh
@@ -4,6 +4,11 @@
 # virtualenv-init depends on using unset variables, so you cannot use -u
 set -ex
 
+# Create GOPATH and add ~/go/bin to the $PATH for ease of using `go get ...`
+mkdir -p ~/go
+echo 'export GOPATH="/home/vagrant/go"' >> ~/.bash_profile
+echo 'export PATH="$PATH:$GOPATH/bin"' >> ~/.bash_profile
+
 # Use pyenv to install python 3.6.4. The apt-based solutions I tried really stunk.
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
 echo 'export PATH="/home/vagrant/.pyenv/bin:$PATH"' >> ~/.bash_profile
