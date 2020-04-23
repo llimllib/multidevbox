0f3b7645a787a334de0c2c0898d89fb6024672b7
Bill Mill <bill@billmill.org>
2020-04-23 10:10:49 -0400

    update provision.sh

--- provision.sh
+++ provision.sh
@@ -3,64 +3,68 @@ set -eux
 
 # get add-apt-repository and curl
 apt-get update
-apt-get install -y software-properties-common curl less zip apt-transport-https libffi-dev clang
+apt-get install -y software-properties-common curl less zip apt-transport-https ca-certificates
 
 # add PL PPAs
-add-apt-repository ppa:ansible/ansible
-add-apt-repository ppa:nginx/stable
-add-apt-repository ppa:ondrej/php
-add-apt-repository ppa:linuxuprising/java
+add-apt-repository ppa:ansible/ansible # ansible
+add-apt-repository ppa:nginx/stable    # nginx
+add-apt-repository ppa:ondrej/php      # php
 
-# https://github.com/ponylang/ponyc/blob/master/README.md#installation
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E04F0923 B3B48BDA
-add-apt-repository "deb https://dl.bintray.com/pony-language/ponylang-debian  $(lsb_release -cs) main"
-
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+curl -sSf https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 
 # elixir
 # https://elixir-lang.org/install.html
-curl -sS https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb -o erlang-solutions_1.0_all.deb \
+curl -sSf https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb -o erlang-solutions_1.0_all.deb \
   && dpkg -i erlang-solutions_1.0_all.deb \
   && rm erlang-solutions_1.0_all.deb
 
 # dotnet instructions modified from
 # https://www.microsoft.com/net/learn/dotnet/hello-world-tutorial
-curl -sS https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -o packages-microsoft-prod.deb \
+curl -sSf https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -o packages-microsoft-prod.deb \
   && dpkg -i packages-microsoft-prod.deb \
   && rm packages-microsoft-prod.deb
 
 apt-get update
 
-# Auto-accept the java license. Oracle sucks.
-# https://stackoverflow.com/a/19391042/42559
-echo debconf shared/accepted-oracle-license-v1-2 select true | debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-2 seen true | debconf-set-selections
-
-apt-get install -y git bash-completion make build-essential libssl-dev \
-  zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget llvm \
-  libncurses5-dev libncursesw5-dev xz-utils tk-dev unzip \
+apt-get install -y \
+  bash-completion \
+  build-essential \
+  xz-utils \
+  tk-dev \
+  libffi-dev \
   ansible \
+  clang \
   dotnet-sdk-2.2 \
+  dotnet-sdk-3.1 \
   elixir \
   esl-erlang \
+  git \
+  gnat \
+  libbz2-dev \
+  libncurses5-dev \
+  libncursesw5-dev \
+  libreadline-dev \
+  libsqlite3-dev \
+  libssl-dev \
+  llvm \
+  make \
   nginx \
-  ponyc \
-  runit
+  runit \
+  unzip \
+  wget \
+  zlib1g-dev
 
 # Need to install php with --no-recommends to prevent apache2 from being installed
 apt-get install --no-install-recommends -y php7.3 php7.3-mbstring
-
 
 # yarn is a special snowflake; we want to install it independent of debian's
 # node, so we have to do this. cf: https://yarnpkg.com/en/docs/install#debian-stable
 apt-get install -y --no-install-recommends yarn
 
-# download and install go 1.11 to /usr/local/go
-curl -sS https://dl.google.com/go/go1.12.1.linux-amd64.tar.gz | tar -C /usr/local -xz
+# download and install go 1.14 to /usr/local/go
+curl -sSf https://dl.google.com/go/go1.14.linux-amd64.tar.gz | tar -C /usr/local -xz
 
 # install leiningen for clojure
-curl -sSL https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein -o /usr/bin/lein
+curl -sSfL https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein -o /usr/bin/lein
 chmod a+x /usr/bin/lein
-
-sudo apt-get -y install ponyc
